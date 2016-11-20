c9f2c69452927e7142f60bb4450e55d43d5f7db4
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2016-11-20 20:50:21 +0100

    rubymotionise the gem

--- lib/motion-json-api.rb
+++ lib/motion-json-api.rb
@@ -1,8 +1,11 @@
-require 'motion-json-api/exceptions'
-require 'motion-json-api/resource'
-
-module MotionJsonApi
-  def self.parse(json)
-    Resource._object_handler(json, json.fetch("data", []), json.fetch("included", []))
+if defined?(Motion::Project::Config)
+  Motion::Project::App.setup do |app|
+    Dir.glob(File.join(File.dirname(__FILE__), "motion-json-api/**/*.rb")).each do |file|
+      app.files.unshift(file)
+    end
   end
+else
+  require 'motion-json-api/exceptions'
+  require 'motion-json-api/descendants'
+  require 'motion-json-api/resource'
 end
