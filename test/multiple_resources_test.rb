456f6d314b37426cbb4d1878eb383b55f623ed1d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2017-01-05 23:58:55 +0100

    updates test for array of resources

--- test/multiple_resources_test.rb
+++ test/multiple_resources_test.rb
@@ -7,15 +7,15 @@ class MultipleResourcesTest < Minitest::Test
   end
 
   def test_attribute
-    assert_equal(@subject.first.name, "joffrey")
+    assert_equal(@subject.data.first.name, "joffrey")
   end
 
   def test_id
-    assert_equal(@subject.first.id, "36ffe4bf-85d6-45d2-8c41-7998c34cafcd")
+    assert_equal(@subject.data.first.id, "36ffe4bf-85d6-45d2-8c41-7998c34cafcd")
   end
 
   def test_included_top_level_data
-    author = @subject.first.favorites.first.recent_contribution.author
+    author = @subject.data.first.favorites.first.recent_contribution.author
     assert_equal(author.class, UserResource)
     assert_equal(author.id, "36ffe4bf-85d6-45d2-8c41-7998c34cafcd")
   end
